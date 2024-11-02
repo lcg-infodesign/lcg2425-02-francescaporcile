509c8bdd79b6d7b4d2b1699632833a4144a4b240
francescaporcile <francesca.porcile@mail.polimi.it>
2024-11-02 18:59:09 +0100

    Update sketch.js
    definizione della griglia, righe e colonne

--- sketch.js
+++ sketch.js
@@ -8,3 +8,14 @@ function setup() {
   background("#ffffff"); //bianco 
   noLoop();
 }
+// Genera un array di colori casuali, ciclo for con 10 colori casuali (lavorando sulle tre componenti del modello colore RGB)
+for (let i = 0; i < 10; i++) {
+  colors.push(color(random(255), random(255), random(255)))
+}
+
+  // Calcola la larghezza e l'altezza disponibili per la griglia, considerando il padding
+  let AvWidth = width - 2 * padding;   // Larghezza totale disponibile
+  let AvHeight = height - 2 * padding; // Altezza totale disponibile
+   // Calcola il numero di colonne e righe in base alle dimensioni disponibili
+   let numColumns = (AvWidth / (glifSize + space)); // Numero di colonne
+   let numRows = (AvHeight / (glifSize + space));   // Numero di righe
